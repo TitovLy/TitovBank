f9ab27ad9eed2b4d232fa1e84b46a3fe3d1da254
TitovLy <leoneed1507@mail.ru>
2024-04-05 16:14:28 +0300

    Make a card object

--- src/main/java/bank/Card.java
+++ src/main/java/bank/Card.java
@@ -1,4 +1,40 @@
 package bank;
 
 public class Card {
+    //    константа
+    private final String CARD_NUMBER;
+    private String pinCode;
+
+    public Card() {
+        CARD_NUMBER = generateCardNumber(16);
+        pinCode = generateCardNumber(4);
+    }
+
+    private int generatePinCode(int min, int max) {
+        max -= min;
+        return(int) (Math.random() * ++max) + min;
+
+    }
+
+    private String generateCardNumber(int length) {
+        StringBuilder builder = new StringBuilder();
+
+        for (int i = 0; i < length; i++) {
+            builder.append(generatePinCode(0,9));
+        }
+        return builder.toString();
+    }
+    public void setPinCode(String pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String getCARD_NUMBER() {
+        return CARD_NUMBER;
+    }
+
+    public String getPinCode() {
+        return pinCode;
+    }
 }
+
+
--- src/main/java/data/Client.java
+++ src/main/java/data/Client.java
@@ -1,4 +1,28 @@
 package data;
 
+import bank.Card;
 public class Client {
+
+    private String name;
+    private String surName;
+    private Card card;
+    private String isClient;
+    public Card getCard(){
+        return card;
+    }
+    public void setCard(Card card){
+        this.card = card;
+    }
+    public String getIsClient(){
+        return isClient;
+    }
+    public void setIsClient(String isClient){
+        this.isClient = isClient;
+    }
+    public String getName(){
+        return name;
+    }
+    public String getSurname(){
+        return surName;
+    }
 }
